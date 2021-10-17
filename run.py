50fc2bbaf79e6aec96538ec846a8f5f7c3d5c618
zainabmeerza <zainabmeerza@gmail.com>
2021-10-17 20:12:00 +0000

    create function that generates random coordinates

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 for random import randint
+
+def random_coordinates(grid_size):
+    """
+    Function that will gernerate a random pair of coordinates within the grid size
+    """
+    x = randint(0, grid_size - 1)
+    y = randint(0, grid_size - 1)
+
+    return (x,y)
