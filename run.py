ed7a1464cbcf6294823dd36701c586d4fc42bf89
zainabmeerza <zainabmeerza@gmail.com>
2021-10-17 20:18:59 +0000

    create function that checks if the entered coordinates are valid

--- run.py
+++ run.py
@@ -1,10 +1,24 @@
 for random import randint
 
+
 def random_coordinates(grid_size):
     """
-    Function that will gernerate a random pair of coordinates within the grid size
+    Function that will gernerate a random pair of
+    coordinates within the grid size.
     """
     x = randint(0, grid_size - 1)
     y = randint(0, grid_size - 1)
 
-    return (x,y)
+    return (x, y)
+
+
+def validate_coordinates(x, y, grid_size):
+    """
+    Function that checks and verifies that the pair of
+    coordinates entered by the player are valid and within the grid size.
+    """
+    if 0 <= x < gird_size and 0 <= y < grid_size:
+        return True
+    
+    return False
+
